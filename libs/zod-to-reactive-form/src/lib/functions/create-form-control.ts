ecf1896709b069ce7d5c904c3e91eaa062ba56db
Alex Skoropad <alex@skoropad.dev>
2024-12-10 16:17:47 +0100

    fix: createControl doesn't handle overrides (#6)

--- libs/zod-to-reactive-form/src/lib/functions/create-form-control.ts
+++ libs/zod-to-reactive-form/src/lib/functions/create-form-control.ts
@@ -1,18 +1,18 @@
-import {GuardUndefined} from '../types';
+import {FieldOptions, GuardUndefined} from '../types';
 import {z} from 'zod';
 import {AbstractControlOptions, FormControl, Validators} from '@angular/forms';
 import {isZodSchemaRequired} from './is-zod-schema-required';
 
-export interface FormControlFromZodOptions<TSchema extends z.ZodType> {
-  value?: GuardUndefined<z.infer<TSchema>>;
-  disabled?: boolean;
-}
-
-export function createFormControl<TSchema extends z.ZodType>(schema: TSchema, overrides?: FormControlFromZodOptions<TSchema>, options?: AbstractControlOptions): FormControl<GuardUndefined<z.infer<TSchema>>> {
+export function createFormControl<TSchema extends z.ZodType>(schema: TSchema, overrides?: FieldOptions<z.infer<TSchema>> & AbstractControlOptions): FormControl<GuardUndefined<z.infer<TSchema>>> {
   const { value = null, disabled = false } = overrides ?? {};
 
   return new FormControl(
     {value, disabled},
-    options ?? {nonNullable: isZodSchemaRequired(schema), validators: isZodSchemaRequired(schema) ? [Validators.required] : []}
+    {
+      nonNullable: isZodSchemaRequired(schema),
+      validators: isZodSchemaRequired(schema) ? [Validators.required] : [],
+      asyncValidators: [],
+      ...overrides
+    }
   ) as FormControl<GuardUndefined<z.infer<TSchema>>>;
 }
